dd6085dea2685e43df52e08401fc20a38febd7e4
shoggothforever <1337231450@qq.com>
2023-07-16 15:01:28 +0800

    complete guessNumber game

--- guessnumber/src/main.rs
+++ guessnumber/src/main.rs
@@ -1,3 +1,30 @@
+use core::num;
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+    let _ans = rand::thread_rng().gen_range(1..=100);
+    loop{
+    let mut guess=String::new();
+    println!("input your guess number");
+    io::stdin().read_line(&mut guess).expect("输入失败");
+    let guess: u32 =match guess.trim().parse() {
+        Ok(num)=>num,
+        Err(_) => {
+            println!("字符串转换成数字类型失败");
+            continue;
+        },
+    };
+    println!("your guessed: {guess}");
+        match guess.cmp(&_ans){
+            Ordering::Greater => println!("猜测数字大了{}",guess-_ans),
+            Ordering::Equal => {
+                println!("猜对了");
+                break;
+            },
+            Ordering::Less => println!("猜测数字小了{}",_ans-guess),
+        }
+    }
 }
+
